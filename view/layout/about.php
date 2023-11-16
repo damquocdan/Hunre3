508be5af83fdc2ab8855aab48acc7092425baeec
devbluddy <saiminhhieu0712@gmail.com>
2023-11-16 16:08:23 +0700

    hieu

--- view/layout/about.php
+++ view/layout/about.php
@@ -23,10 +23,7 @@
                 <a href="#" class="about-column-link">Định nghĩa</a>
               </div>
               <div class="about-column-item">
-                <a href="#" class="about-column-link">Chính sách trả góp</a>
-              </div>
-              <div class="about-column-item">
-                <a href="#" class="about-column-link">Chính sách đổi trả phòng</a>
+                <a href="#" class="about-column-link">Chính sách trả phòng</a>
               </div>
               <div class="about-column-item">
                 <a href="#" class="about-column-link">Chính sách bảo mật</a>
@@ -50,34 +47,13 @@
               </h2>
 
               <p class="about-content-desc">
-                Công ty TNHH Một Thành Viên Thương Mại Thời Trang Tổng Hợp
-          
+                Traveloka được thành lập vào năm 2023 bởi các học viên công nghệ thông tin cuar DH11C12, bao gồm ĐÀM QUỐC DÂN, NGUYỄN KHƯƠNG DUY, SÁI MINH HIẾU và BÙI NGỌC BÁCH
+
               </p>
-
-              <div class="about-img">
-                <img src="././public/images/about-01.webp" alt="" class="about-image" />
-                <span>ShoeLike sở hữu đội ngũ trẻ trung, năng động</span>
-              </div>
-
               <p class="about-content-desc">
-                ShoeLike Vietnam là một hệ thống thuộc tập đoàn Central
-                Retail hoạt động theo hai hình thức bán lẻ chính:
+                Ban đầu, Traveloka đóng vai trò như một công cụ tìm kiếm để so sánh giá phòng từ nhiều trang khác nhau. Sau đó, vào giữa năm 2023, Traveloka đã biến thành một trang web đặt phòng, nơi người dùng có thể đặt chỗ trên trang web chính thức của hãng.
               </p>
-
-              <ul class="about-content-list">
-                <li class="about-content-item">
-                  Cửa hàng bán lẻ đồ thể thao của các thương hiệu khác
-                  nhau có tên là “ShoeLike”
-                </li>
-                <li class="about-content-item">
-                  Cửa hàng chuyên biệt thương hiệu như Crocs, Under
-                  Armour, New Balance, Fila và Skechers.
-                </li>
-                <p class="about-content-desc">
-                  Sở hữu hơn 1200 địa điểm phân phối chính hãng các sản
-                 
-                </p>
-              </ul>
+              <p class="about-content-desc"> Sở hữu hơn 200 phòng trên đia bàn Hà Nội</p>
 
               <h2 class="about-content-title">2. Sứ mệnh và tầm nhìn</h2>
 
@@ -85,14 +61,12 @@
                 <span class="bold"> Tầm nhìn </span>
               </p>
               <p class="about-content-desc">
-                ShoeLike mong muốn “Trở thành Nhà bán lẻ & phân phối
-                thương hiệu các mặt hàng về thể thao tốt nhất trên thế
-                giới”.
+                TheHouse mong muốn “Trở thành Công ty cho thuê phòng tốt nhất tại Việt Nam”.
               </p>
 
               <div class="about-img">
                 <img src="././public/images/about-02.webp" alt="" class="about-image" />
-                <span>Chúng tôi luôn nỗ lực mang những sản phẩm chính hãng
+                <span>Chúng tôi luôn nỗ lực mang những căn phòng tốt nhất
                   đến tay bạn</span>
               </div>
 
@@ -101,152 +75,84 @@
               </p>
 
               <p class="about-content-desc">
-                ShoeLike Vietnam luôn nỗ lực để cung cấp các sản phẩm
-                
+                TheHouse Vietnam luôn nỗ lực để cung cấp phòng tốt nhất về chất lượng cũng như giá cả tới tất cả khách hàng. Luôn ưu tiên đặt chữ tín lên hàng đầu
+
               </p>
 
               <h2 class="about-content-title">
-                3. Các cam kết của ShoeLike
+                3. Các cam kết của TheHouse
               </h2>
 
               <h3 class="about-content-heading">
-                Cam kết với người mua hàng
+                Cam kết với người thuê
               </h3>
               <p class="about-content-desc">
-                Đến với ShoeLike, bạn sẽ nhận được những cam kết sau
+                Đến với TheHouse, bạn sẽ nhận được những cam kết sau
                 của chúng tôi:
               </p>
               <ul class="about-content-list">
                 <li class="about-content-item">
-                  Cam kết sản phẩm chính hãng với giá tốt nhất.
+                  Cam kết phòng đẹp với giá tốt nhất.
                 </li>
                 <li class="about-content-item">
-                  Cung cấp trải nghiệm mua sắm tốt nhất.
+                  Cung cấp trải nghiệm chỗ ở tốt nhất.
                 </li>
                 <li class="about-content-item">
-                  Chính sách bảo hành, đổi trả và hoàn tiền rõ ràng
+                  Chính sách bảo hành, hoàn tiền rõ ràng
                 </li>
                 <li class="about-content-item">
                   Luôn lắng nghe khách hàng nhằm hỗ trợ kịp thời và cải
                   thiện chất lượng dịch vụ. Vì thế, bạn có thể hoàn toàn
-                  yên tâm khi mua sắm tại 17 cửa hàng trong hệ thống của
-                  ShoeLike về cả chất lượng sản phẩm lẫn chất lượng
+                  yên tâm khi thuê phòng của
+                  TheHouse về cả chất lượng phòng lẫn chất lượng
                   dịch vụ.
                 </li>
               </ul>
 
               <h3 class="about-content-heading">Cam kết với cộng đồng</h3>
               <p class="about-content-desc">
-                ShoeLike Vietnam nói riêng và tập đoàn Central Retail
-                nói chung luôn nỗ lực đóng góp vào sự thịnh vượng của đất
+                TheHouse luôn nỗ lực đóng góp vào sự thịnh vượng của đất
                 nước và nâng cao chất lượng cuộc sống của người Việt.
-                Chúng tôi luôn cố gắng mở rộng mối liên kết giữa những
-                người đam mê thể thao tại Vietnam và tạo ra một thế giới
-                bền vững. Ngoài ra, chúng tôi luôn quan tâm cải thiện chất
+                Ngoài ra, chúng tôi luôn quan tâm cải thiện chất
                 lượng môi trường và hướng tới xây dựng cuộc sống hạnh phúc
-                thông qua việc cải thiện và nâng cao sức khỏe ở các thế hệ
+                thông qua việc cải thiện và nâng cao nơi ở các thế hệ
                 người Việt.
               </p>
 
               <h2 class="about-content-title">
-                4. Các dòng sản phẩm tại ShoeLike Vietnam
+                4. Các loại phòng hiện tại của TheHouse Vietnam
               </h2>
 
               <h3 class="about-content-heading">
-                <span class="bold">Giày dép</span>
+                <span class="bold">Hiện tại, TheHouse cung cấp các loại phòng
+              /span>
               </h3>
 
-              <p class="about-content-desc">
-                Hiện tại, ShoeLike Vietnam cung cấp các dòng sản phẩm
-                chính:
-              </p>
 
               <ul class="about-content-list">
                 <li class="about-content-item">
-                  Giày thể thao: Bao gồm các môn bóng đá, bóng rổ, chạy
-                  bộ, thể thao ngoài trời - trong nhà,...
+                  Phòng mới
                 </li>
                 <li class="about-content-item">
-                  Giày thời trang: Các mẫu giày thời trang đến từ thương
-                  hiệu New Balance, Adidas, Converse,...
+                 Phòng sinh viên
                 </li>
                 <li class="about-content-item">
-                  Xăng đan và dép: Supersport cung cấp đa dạng các mẫu
-                  xăng đan đến từ Teva, Hoka, Puma,...
+                 chung cư mini
+                </li>
+                <li class="about-content-item">
+                 căn hộ
+                </li>
+                <li class="about-content-item">
+                 mặt bằng kinh doanh
                 </li>
               </ul>
-
-              <div class="about-img">
-                <img src="././public/images/about-03.webp" alt="" class="about-image" />
-                <span>ShoeLike sở hữu đội ngũ trẻ trung, năng động</span>
-              </div>
-
-              <h3 class="about-content-heading">Trang phục</h3>
-
-              <p class="about-content-desc">
-                ShoeLike Vietnam mang đến cho bạn những mẫu trang phục
-                thể thao và thời trang cao cấp nhất, cụ thể:
-              </p>
-
-              <ul class="about-content-list">
-                <li class="about-content-item">
-                  Các mẫu quần đến từ các thương hiệu Under Armour,
-                  Converse, LFC,...
-                </li>
-                <li class="about-content-item">
-                  Các mẫu áo từ New Balance, Nike, Venum,...
-                </li>
-              </ul>
-
-              <div class="about-img">
-                <img src="././public/images/about-04.webp" alt="" class="about-image" />
-                <span>Các trang phục thể thao tại ShoeLike </span>
-              </div>
-
-              <h3 class="about-content-heading">Dụng cụ</h3>
-
-              <p class="about-content-desc">
-                Chúng tôi cung cấp đa dạng các loại dụng cụ phù hợp cho
-                các môn thể thao:
-              </p>
-
-              <ul class="about-content-list">
-                <li class="about-content-item">
-                  Bóng đá: Bóng, vớ, túi đeo, găng tay, mũ,...
-                </li>
-                <li class="about-content-item">
-                  Bóng rổ: Bóng, vớ, nón,...
-                </li>
-                <li class="about-content-item">
-                  Yoga: Bóng yoga, dây và thảm tập
-                </li>
-                <li class="about-content-item">
-                  Đồ bảo hộ: Các dụng cụ bảo vệ tay, chân,... khi luyện
-                  tập thể thao
-                </li>
-                <li class="about-content-item">
-                  Dụng cụ hỗ trợ tập luyện thể thao: Con lăn, bánh xe,
-                  bóng mát xa,...
-                </li>
-                <li class="about-content-item">
-                  Golf: Áo, các loại quần, dù,...
-                </li>
-              </ul>
-
-              <div class="about-img">
-                <img src="././public/images/about-05.webp" alt="" class="about-image" />
-                <span>Đa dạng những phòng khác nhau</span>
-              </div>
-
               <h2 class="about-content-title">
                 5. Các khu vực chính:
               </h2>
 
               <p class="about-content-desc">
-                TheHouse là nhà phân phối chính thức các phòng đến
-                từ những khu vực đông đúc và tiện lợi giao thông khu vực Hà Nội:
+                TheHouse là công ty cho thuê tại những khu vực đông đúc và tiện lợi giao thông khu vực Hà Nội:
               </p>
-
               <ul class="about-content-list">
                 <li class="about-content-item">Ba Đình</li>
                 <li class="about-content-item">Hà Đông</li>
@@ -259,12 +165,6 @@
                   Và một số khu vực khác
                 </li>
               </ul>
-
-              <p class="about-content-desc">
-                Với hơn 20 năm kinh nghiệm trong việc cung cấp các phòng ở cho mọi người, TheHouse tin rằng
-                bạn sẽ hài lòng khi đến đặt thuê phòng tại hơn 15 văn phòng của
-                chúng tôi trên toàn quốc.
-              </p>
             </div>
           </div>
         </article>
